6df640d79070ca8987f19e6a850aa05d611091ee
6awdsasdaw9 <69734315+6awdsasdaw9@users.noreply.github.com>
2023-03-08 00:35:19 +0300

    RocketMove refactoring

--- Assets/Code/RocketMove.cs
+++ Assets/Code/RocketMove.cs
@@ -1,3 +1,4 @@
+using System;
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
@@ -6,15 +7,22 @@ public class RocketMove : MonoBehaviour
 {
     [SerializeField] private InputController inputController;
     [SerializeField] private Rigidbody2D _rb;
-    [SerializeField] float _speed = 5;
-    [SerializeField] float _maxSpeed = 15;
+    [SerializeField,Range(0.5f,5)] private float _speed = 1;
+    [SerializeField,Range(1,10)] private float _maxSpeed = 2.8f;
 
+    private IObservable<Unit> _updateObservable;
     private void OnEnable()
     {
         inputController.OnPressMove += MoveToForward;
-        this.UpdateAsObservable().Subscribe(_ => TurnForward());
-        this.UpdateAsObservable().Subscribe(_ => ScreenWrap());
-        this.UpdateAsObservable().Subscribe(_ => Acceleration());
+        
+        _updateObservable = this.UpdateAsObservable().Share();
+        
+        _updateObservable
+            .Subscribe(_ => TurnForward())
+            .AddTo(this);
+        _updateObservable
+            .Subscribe(_ => ScreenWrap())
+            .AddTo(this);
     }
 
     private void OnDisable()
@@ -24,40 +32,43 @@ public class RocketMove : MonoBehaviour
     
     private void TurnForward()
     {
-        var direction = inputController.mousePosition - (Vector2)transform.position;
-        transform.up = direction;
+        Vector2 direction = inputController.mousePosition - (Vector2)transform.position;
+        transform.up = direction.normalized;
     }
 
     private void Acceleration()
     {
-        if (_rb.velocity.y > _maxSpeed)
-        {
-            _rb.velocity = new Vector2(_rb.velocity.x, _maxSpeed);
-        }
-        if (_rb.velocity.x > _maxSpeed)
-        {
-            _rb.velocity = new Vector2( _maxSpeed,_rb.velocity.y);
-        }
+        _rb.velocity = new Vector2(Mathf.Clamp(_rb.velocity.x, -_maxSpeed, _maxSpeed),
+            Mathf.Clamp(_rb.velocity.y, -_maxSpeed, _maxSpeed));
     }
+    
     private void MoveToForward()
     {
-        _rb.AddForce(transform.up.normalized * _speed);
+        _rb.AddForce(transform.up * _speed);
+        _rb.velocity = Vector2.ClampMagnitude(_rb.velocity, _maxSpeed);
     }
-
     private void ScreenWrap()
     {
         Vector3 position = Camera.main.WorldToScreenPoint(transform.position);
-        if (position.x > Screen.width || position.x < 0)
+
+        if (position.x > Screen.width)
         {
-            position.x = position.x > Screen.width ? 0 : Screen.width;
-            transform.position = Camera.main.ScreenToWorldPoint(position);
+            position.x %= Screen.width;
+        }
+        else if (position.x < 0)
+        {
+            position.x += Screen.width;
         }
 
-        if (position.y > Screen.height || position.y < 0)
+        if (position.y > Screen.height)
         {
-            position.y = position.y > Screen.height ? 0 : Screen.height;
-            transform.position = Camera.main.ScreenToWorldPoint(position);
+            position.y %= Screen.height;
+        }
+        else if (position.y < 0)
+        {
+            position.y += Screen.height;
         }
 
+        transform.position = Camera.main.ScreenToWorldPoint(position);
     }
 }
